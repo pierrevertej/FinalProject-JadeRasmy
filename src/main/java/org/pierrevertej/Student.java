620dadd32dd3e7e83968bea8034eabb0a489dd1d
pierrevertej <pierrevertej@gmail.com>
2025-12-15 20:11:49 -0500

    did student methods that dont involve course

---
+++ src/main/java/org/pierrevertej/Student.java
@@ -0,0 +1,39 @@
+package org.pierrevertej;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    static int nextId = 0;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.studentId = String.format("%05d", ++nextId);
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    public String toSimplifiedString() {
+        return "{studentId=" + studentId +
+                ", studentName=" + studentName +
+                ", departmentName=" + department.getDepartmentName() + "}";
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
