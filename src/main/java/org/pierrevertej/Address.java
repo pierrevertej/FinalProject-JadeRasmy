629e178fe31b63f7d450093c8a8e748def923d36
pierrevertej <pierrevertej@gmail.com>
2025-12-15 19:46:15 -0500

    finish address

--- src/main/java/org/pierrevertej/Address.java
+++ src/main/java/org/pierrevertej/Address.java
@@ -1,5 +1,14 @@
 package org.pierrevertej;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Address {
     private int streetNo;
     private String street;
